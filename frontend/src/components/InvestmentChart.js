4454ad1591ba8fa801ebaf28880d786079b02a9a
Derek <RumbleInCybertron@users.noreply.github.com>
2024-09-20 00:20:19 +0900

    + invesment chart shell component

---
+++ frontend/src/components/InvestmentChart.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
+import axios from 'axios';
+
+const InvestmentChart = ({ investmentId}) => {
+
+  return (
+    <div></div>
+  );
+};
+
+export default InvestmentChart;
