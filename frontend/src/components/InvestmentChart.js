cf6da5d72a35cfebaf094692f2f7caf8d310a5d3
Derek <RumbleInCybertron@users.noreply.github.com>
2024-09-21 17:09:22 +0900

    + chart cmpnt

--- frontend/src/components/InvestmentChart.js
+++ frontend/src/components/InvestmentChart.js
@@ -2,10 +2,35 @@ import React, { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import axios from 'axios';
 
-const InvestmentChart = ({ investmentId}) => {
+const InvestmentChart = ({ investmentId }) => {
+  const [chartData, setChartData] = useState({});
+
+  useEffect(() => {
+    axios.get(`/api/history/?investment=${investmentId}`)
+      .then(response => {
+        const dates = response.data.map(record => record.date);
+        const values = response.data.map(record => record.values);
+
+        setChartData({
+          labels: dates,
+          datasets: [
+            {
+              label: 'Investment Value Over Time',
+              data: values,
+              borderColor: 'rgba(75,192,192,1)',
+              fill: false,
+            },
+          ],
+        });
+      })
+      .catch(err => console.log(err));
+  }, [investmentId]);
 
   return (
-    <div></div>
+    <div>
+      <h2>투자 성과</h2>
+      <Line data={chartData} />
+    </div>
   );
 };
 
