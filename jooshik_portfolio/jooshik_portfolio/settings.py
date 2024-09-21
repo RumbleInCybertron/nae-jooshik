b4ffffe3f85f3f4bf8b0e5e188a958580e3a4c00
Derek <RumbleInCybertron@users.noreply.github.com>
2024-09-21 17:10:20 +0900

    + cors

--- jooshik_portfolio/jooshik_portfolio/settings.py
+++ jooshik_portfolio/jooshik_portfolio/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'portfolio',
 ]
 
@@ -39,7 +40,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'jooshik_portfolio.urls'
 
